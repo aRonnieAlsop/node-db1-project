a24f0076143a47e8c7040a0e26a0afb42ffcea8c
Arron Alsop <alsop.arron@gmail.com>
2023-12-07 12:57:33 -0800

    'wip;

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,30 +1,25 @@
 const db = require('../../data/db-config')
 
 const getAll = () => {
-  // DO YOUR MAGIC
   return db('accounts')
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
-
   return db('accounts').where('id', id).first()
 }
 
-async function create({ name, budget}) {
-  const [id] = await db('accounts').insert({ name, budget })
+const create = async account => {
+  const [id] = await db('accounts').insert(account)
   return getById(id)
 }
 
-async function updateById(id, { name, budget }) {
-  await db('accounts').where('id', id).update({ name, budget })
+const updateById = async (id, account) => {
+  await db('accounts').where('id', id).update(account)
   return getById(id)
 }
 
-async function deleteById(id) {
-  const toBeDeleted = await getById(id)
-  await db('users').where('id', id).delete()
-  return toBeDeleted
+const deleteById = id => {
+  return db('users').where('id', id).delete()
 }
 
 
