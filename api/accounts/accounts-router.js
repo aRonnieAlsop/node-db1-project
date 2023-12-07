ba8af98a06f6b69d8b4926de17a67e72e4e5a66c
Arron Alsop <alsop.arron@gmail.com>
2023-12-07 11:34:19 -0800

    'wip'

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,63 @@
+const Account = require('./accounts-model')
+const { 
+        checkAccountId, 
+        checkAccountNameUnique, 
+        checkAccountPayload
+      } = require('./accounts-middleware')
+
 const router = require('express').Router()
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const accounts = await Account.getAll()
+    res.json(accounts)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, async (req, res) => {
   // DO YOUR MAGIC
+  res.status(200).json(id)
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAccountPayload, checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const newAccount = await Account.create(req.body)
+    res.status(201).json(newAccount)
+  } catch(err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', checkAccountPayload, checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
-});
+  try {
+    const updatedAccount = await Account.update(req.params.id, req.body)
+    res.status(200).json(updatedAccount)
+  } catch(err) {
+    next(err)
+  }
+})
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAccountPayload, checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const deletedAccount = Account.delete(req.params.id)
+    res.status(200).json(deletedAccount)
+  } catch(err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 module.exports = router;
